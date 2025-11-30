8a62ef2d291dd8ad35388947f4b7d4111d2b5343
josean-ralat <josean.ralat@upr.edu>
2025-11-30 13:09:52 -0400

    moreee

--- api/index.py
+++ api/index.py
@@ -10,13 +10,15 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://josean-ralat-portfolio.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "https://josean-ralat-portfolio.vercel.app"
-    ],
+    allow_origins= origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
